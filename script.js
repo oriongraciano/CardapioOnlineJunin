d90274e6370db18354d767d75c6a1a74921d0c96
Orion Graciano <87495924+oriongraciano@users.noreply.github.com>
2024-12-20 13:35:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -185,7 +185,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 24;
+    return hora >= 24 && hora < 24;
 }
 
 const spanItem = document.getElementById("date-span")
